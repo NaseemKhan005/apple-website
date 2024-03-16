eb17ee124ee74809709f982f3428f688d4e67a33
Naseem Khan <naseemsaffy@gmail.com>
2024-03-16 17:22:10 +0500

    app structure done

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+import Highlights from "./components/Highlights";
+import Navbar from "./components/Navbar";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <main className="bg-black">
+      <Navbar />
+      <Hero />
+      <Highlights />
+      <Footer />
+    </main>
+  );
 };
 
 export default App;
